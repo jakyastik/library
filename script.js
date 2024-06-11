ec032c6c53d62631b4e038b639b1b9f3e4c2ac07
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-06-11 12:18:46 +0530

    Adding new books is not working

--- script.js
+++ script.js
@@ -3,21 +3,18 @@ const myLibrary = [
 		name: "For Whom the Bells Toll",
 		author: "Ernest Hemingway",
 		pages: 79,
-
 		read: true,
 	},
 	{
 		name: "Deep Work",
 		author: "Carl Newport",
 		pages: 224,
-
 		read: true,
 	},
 	{
 		name: "Foundation",
 		author: "Isaac Asimov",
 		pages: 189,
-
 		read: false,
 	},
 ];
@@ -33,9 +30,19 @@ function displayBooks() {
 		bookContainer.innerHTML = book.name;
 		bookContainer.append(infoSection);
 		infoSection.setAttribute("class", "infoSection");
-		infoSection.innerHTML = `Written by ${book.author}. Length is ${book.pages} Pages. <br> <p>${book.note}<p>`;
+		infoSection.innerHTML = `Written by <b>${book.author}</b>.<br> ${book.pages} Pages.`;
 
 		libraryContainer.appendChild(bookContainer);
+
+		//add a delete button
+		const deleteButton = document.createElement("button");
+		deleteButton.innerHTML = "Remove";
+		deleteButton.setAttribute("class", "deleteButton");
+		bookContainer.appendChild(deleteButton);
+
+		deleteButton.addEventListener("click", (event) => {
+			removeBookByIndex();
+		});
 	});
 }
 
@@ -57,8 +64,7 @@ addBookToLibrary.addEventListener("click", () => {
 	let bookName = document.getElementById("bookName").value;
 	let authorName = document.getElementById("bookAuthor").value;
 	let pageCount = document.getElementById("totalPages").value;
-	let readStatus = document.getElementById("checkBox").value;
-	console.log(bookName, authorName, pageCount, readStatus);
+	let readStatus = document.getElementById("checkBox").checked;
 
 	const newBook = new Book(
 		(name = bookName),
@@ -68,6 +74,7 @@ addBookToLibrary.addEventListener("click", () => {
 	);
 
 	myLibrary.push(newBook);
+	displayBooks();
 });
 
 // Show Hide the Add Section //
@@ -80,4 +87,15 @@ function openForm() {
 function closeForm() {
 	let addForm = document.querySelector(".addForm");
 	addForm.classList.toggle("closeToggle");
+}
+
+closeForm();
+
+function removeBookByIndex(index) {
+	if (index >= 0 && index < myLibrary.length) {
+		myLibrary.splice(index, 1);
+	} else {
+		console.log("Index out of bounds");
+	}
+	displayBooks();
 }
