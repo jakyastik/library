62719e173b9535eef9db5069c370c4c392e9388f
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-06-11 10:15:17 +0530

    added more functionality to code

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-const myLibrary = [];
+const myLibrary = [
+	{
+		name: "For Whom the Bells Toll",
+		author: "Ernest Hemingway",
+		pages: 79,
+
+		read: true,
+	},
+	{
+		name: "Deep Work",
+		author: "Carl Newport",
+		pages: 224,
+
+		read: true,
+	},
+	{
+		name: "Foundation",
+		author: "Isaac Asimov",
+		pages: 189,
+
+		read: false,
+	},
+];
+
+function displayBooks() {
+	const libraryContainer = document.querySelector(".bookList");
+	libraryContainer.innerHTML = "";
+
+	myLibrary.forEach((book) => {
+		const bookContainer = document.createElement("div");
+		const infoSection = document.createElement("div");
+		bookContainer.setAttribute("class", "bookDiv");
+		bookContainer.innerHTML = book.name;
+		bookContainer.append(infoSection);
+		infoSection.setAttribute("class", "infoSection");
+		infoSection.innerHTML = `Written by ${book.author}. Length is ${book.pages} Pages. <br> <p>${book.note}<p>`;
+
+		libraryContainer.appendChild(bookContainer);
+	});
+}
+
+displayBooks();
 
 class Book {
 	constructor(name, author, pages, read) {
@@ -10,14 +51,23 @@ class Book {
 	}
 }
 
-let addBookToLibrary = document.querySelector("#submitButton");
+let addBookToLibrary = document.getElementById("submitButton");
+
 addBookToLibrary.addEventListener("click", () => {
 	let bookName = document.getElementById("bookName").value;
 	let authorName = document.getElementById("bookAuthor").value;
-	let pages = document.getElementById("totalPages").value;
-	let read = document.getElementById("checkBox").value;
+	let pageCount = document.getElementById("totalPages").value;
+	let readStatus = document.getElementById("checkBox").value;
+	console.log(bookName, authorName, pageCount, readStatus);
 
-	console.log(bookName, authorName, pages, read);
+	const newBook = new Book(
+		(name = bookName),
+		(author = authorName),
+		(pageCount = pageCount),
+		(read = readStatus)
+	);
+
+	myLibrary.push(newBook);
 });
 
 // Show Hide the Add Section //
