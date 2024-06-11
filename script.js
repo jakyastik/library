17d1d66c9cc6036c8858089b1facc40622d8945d
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-06-11 19:15:59 +0530

    Added book covers and delete button functionality to the app

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const myLibrary = [
 	{
-		name: "For Whom the Bells Toll",
+		name: "For Whom the Bell Tolls",
 		author: "Ernest Hemingway",
+		coverUrl:
+			"https://upload.wikimedia.org/wikipedia/en/thumb/4/48/ErnestHemmingway_ForWhomTheBellTolls.jpg/178px-ErnestHemmingway_ForWhomTheBellTolls.jpg",
 		pages: 79,
 		read: true,
 	},
 	{
 		name: "Deep Work",
 		author: "Carl Newport",
+		coverUrl:
+			"https://booksandyou.in/cdn/shop/files/DeepWorkCalNewport_1_2.jpg",
 		pages: 224,
 		read: true,
 	},
 	{
 		name: "Foundation",
 		author: "Isaac Asimov",
+		coverUrl:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKvfKxS48_gMjbPXtN8Oyxvboguy6hsQ3AqA&s",
+		pages: 189,
+		read: false,
+	},
+	{
+		name: "Gone With the Wind",
+		author: "Margaret Mitchell",
+		coverUrl:
+			"https://m.media-amazon.com/images/I/612gOvRX85L._AC_UF1000,1000_QL80_.jpg",
+		pages: 542,
+		read: false,
+	},
+	{
+		name: "Life of Pi",
+		author: "Yann Martel",
+		coverUrl:
+			"https://m.media-amazon.com/images/I/816NlEQFMOL._AC_UF1000,1000_QL80_.jpg",
 		pages: 189,
 		read: false,
 	},
@@ -27,10 +49,21 @@ function displayBooks() {
 		const bookContainer = document.createElement("div");
 		const infoSection = document.createElement("div");
 		bookContainer.setAttribute("class", "bookDiv");
-		bookContainer.innerHTML = book.name;
+
+		//insert book image
+
+		const coverContainer = document.createElement("div");
+		const coverImage = document.createElement("img");
+
+		bookContainer.appendChild(coverContainer);
+		coverContainer.append(coverImage);
+		coverImage.setAttribute("src", book.coverUrl);
+		coverImage.setAttribute("class", "coverImage");
+
+		//insertbook name and info.
 		bookContainer.append(infoSection);
 		infoSection.setAttribute("class", "infoSection");
-		infoSection.innerHTML = `Written by <b>${book.author}</b>.<br> ${book.pages} Pages.`;
+		infoSection.innerHTML = `<h2>${book.name}</h2> <br><b>${book.author}</b>.<br> ${book.pages} Pages.`;
 
 		libraryContainer.appendChild(bookContainer);
 
@@ -40,8 +73,21 @@ function displayBooks() {
 		deleteButton.setAttribute("class", "deleteButton");
 		bookContainer.appendChild(deleteButton);
 
-		deleteButton.addEventListener("click", (event) => {
-			removeBookByIndex();
+		deleteButton.addEventListener("click", () => {
+			libraryContainer.removeChild(bookContainer);
+		});
+
+		//add a read toggle button
+		const toggleButton = document.createElement("button");
+		bookContainer.appendChild(toggleButton);
+		toggleButton.setAttribute("class", "toggleButton");
+
+		toggleButton.addEventListener("click", () => {
+			if (book.read == true) {
+				toggleButton.innerHTML = "Mark as Unread";
+			} else {
+				toggleButton.innerHTML = "Mark as Read";
+			}
 		});
 	});
 }
@@ -49,11 +95,12 @@ function displayBooks() {
 displayBooks();
 
 class Book {
-	constructor(name, author, pages, read) {
+	constructor(name, author, pages, coverUrl, read) {
 		// the constructor
 		this.name = name;
 		this.author = author;
 		this.pages = pages;
+		this.coverUrl = coverUrl;
 		this.read = read;
 	}
 }
@@ -64,16 +111,20 @@ addBookToLibrary.addEventListener("click", () => {
 	let bookName = document.getElementById("bookName").value;
 	let authorName = document.getElementById("bookAuthor").value;
 	let pageCount = document.getElementById("totalPages").value;
+	let coverUrl = document.getElementById("coverUrl").value;
 	let readStatus = document.getElementById("checkBox").checked;
 
 	const newBook = new Book(
 		(name = bookName),
 		(author = authorName),
 		(pageCount = pageCount),
+		(coverUrl = coverUrl),
 		(read = readStatus)
 	);
 
-	myLibrary.push(newBook);
+	console.log(newBook);
+
+	myLibrary.unshift(newBook);
 	displayBooks();
 });
 
@@ -90,12 +141,3 @@ function closeForm() {
 }
 
 closeForm();
-
-function removeBookByIndex(index) {
-	if (index >= 0 && index < myLibrary.length) {
-		myLibrary.splice(index, 1);
-	} else {
-		console.log("Index out of bounds");
-	}
-	displayBooks();
-}
